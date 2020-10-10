8b9c2f48ac1f347a67da6f6e783d217d0ba1612e
Shankar Kulumani <shanks.k@gmail.com>
2020-10-10 09:28:20 -0400

    Update automation

--- record_delete.sh
+++ record_delete.sh
@@ -8,4 +8,4 @@ HQDIR="/media/data/video/high" # Do not include tail '/'
 ## Delete LQ files older than 7 days
 find ${LQDIR} -maxdepth 1 -type f -mtime 7 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
-find ${HQDIR} -maxdepth 1 -type f -mmin 720 -exec rm {} \;
+find ${HQDIR} -maxdepth 1 -type f -mtime 0.5 -exec rm {} \;
