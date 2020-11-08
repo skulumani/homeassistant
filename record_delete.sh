cadf87f3b03e1092b8f61350ece9c70b0f38c425
Shankar Kulumani <shanks.k@gmail.com>
2020-11-08 15:31:04 -0500

    Update crontab

--- record_delete.sh
+++ record_delete.sh
@@ -6,6 +6,6 @@ LQDIR="/media/data/video/low" # Do not include tail '/' (bash 'find' command wil
 HQDIR="/media/data/video/high" # Do not include tail '/'
 
 ## Delete LQ files older than 7 days
-find ${LQDIR} -maxdepth 1 -type f -mtime 7 -exec rm {} \;
+find ${LQDIR} -maxdepth 1 -type f -mtime 5 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
 find ${HQDIR} -maxdepth 1 -type f -mtime 0.5 -exec rm {} \;
