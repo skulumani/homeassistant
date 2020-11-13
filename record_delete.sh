15b43e21c5ccc50cdb83ff125531a86d09d4c49c
Shankar Kulumani <shanks.k@gmail.com>
2020-11-12 19:46:38 -0500

    Rework delete timespan

--- record_delete.sh
+++ record_delete.sh
@@ -6,6 +6,6 @@ LQDIR="/media/data/video/low" # Do not include tail '/' (bash 'find' command wil
 HQDIR="/media/data/video/high" # Do not include tail '/'
 
 ## Delete LQ files older than 7 days
-find ${LQDIR} -maxdepth 1 -type f -mtime 5 -exec rm {} \;
+find ${LQDIR} -maxdepth 1 -type f -mtime 2 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
-find ${HQDIR} -maxdepth 1 -type f -mtime 0.5 -exec rm {} \;
+find ${HQDIR} -maxdepth 1 -type f -mmin 720 -exec rm {} \;
