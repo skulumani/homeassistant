57ba313bf3bd0d8be4cd9792039d6fb686b98823
Shankar Kulumani <shanks.k@gmail.com>
2020-11-30 09:16:03 -0500

    Record more low quality video

--- record_delete.sh
+++ record_delete.sh
@@ -6,6 +6,6 @@ LQDIR="/media/shankar/usbdrive/video/low" # Do not include tail '/' (bash 'find'
 HQDIR="/media/shankar/usbdrive/video/high" # Do not include tail '/'
 
 ## Delete LQ files older than 7 days
-find ${LQDIR} -maxdepth 1 -type f -mtime +1 -exec rm {} \;
+find ${LQDIR} -maxdepth 1 -type f -mtime +2 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
 find ${HQDIR} -maxdepth 1 -type f -mtime +0 -exec rm {} \;
