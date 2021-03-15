afbd4457f5b102b6fcaa634bb384d91571f115ac
Shankar Kulumani <shanks.k@gmail.com>
2021-03-15 08:31:31 -0400

    Switch back to FFMPEG recording and shut off frigate

--- record_delete.sh
+++ record_delete.sh
@@ -5,7 +5,7 @@
 LQDIR="/media/shankar/usbdrive/video/low" # Do not include tail '/' (bash 'find' command will not execute property)
 HQDIR="/media/shankar/usbdrive/video/high" # Do not include tail '/'
 
-## Delete LQ files older than 7 days
-find ${LQDIR} -maxdepth 1 -type f -mtime +3 -exec rm {} \;
+## Delete LQ files older than 7 days -mtime X is files strictly greater than X days old
+find ${LQDIR} -maxdepth 1 -type f -mtime +4 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
 find ${HQDIR} -maxdepth 1 -type f -mtime +1 -exec rm {} \;
--- record_lq.sh
+++ record_lq.sh
@@ -33,3 +33,6 @@ LQLENGTH="3600" # (Runtime expressed in seconds)
 #
 ffmpeg -v 0 -rtsp_transport tcp -i "rtsp://${CAM_USERNAME}:${CAM_PASSWORD}@${FRONT_IP}:554/h264Preview_01_sub" -vcodec copy -acodec copy -t ${LQLENGTH} ${LQDIR}${FRONT_CAM}.mp4 &
 ffmpeg -v 0 -rtsp_transport tcp -i "rtsp://${CAM_USERNAME}:${CAM_PASSWORD}@${BACK_IP}:554/h264Preview_01_sub" -vcodec copy -acodec copy -t ${LQLENGTH} ${LQDIR}${BACK_CAM}.mp4 &
+
+        # - path: rtmp://{FRIGATE_CAM_USERNAME}:{FRIGATE_CAM_PASSWORD}@{FRIGATE_FRONT_CAM_IP}/bcs/channel0_main.bcs?token=sdasdasd&channel=0&stream=0&user={FRIGATE_CAM_USERNAME}&password={FRIGATE_CAM_PASSWORD}
+        # - path: rtmp://{FRIGATE_CAM_USERNAME}:{FRIGATE_CAM_PASSWORD}@{FRIGATE_FRONT_CAM_IP}/bcs/channel0_sub.bcs?token=sdasdasd&channel=0&stream=0&user={FRIGATE_CAM_USERNAME}&password={FRIGATE_CAM_PASSWORD}
