3e550375e324c0b6240043d80bc146bfadcf3770
Shankar Kulumani <shanks.k@gmail.com>
2021-06-01 10:11:12 -0400

    Update influx db functions

---
+++ write_location.py
@@ -0,0 +1,14 @@
+# Module to read Google takeout location history and write to influxdb
+
+# Measurement - location
+# Field key- latitude, longitude, accuracy
+# Field values - Float degrees or meters accuracy
+# tag key - name, activity or other metadata from google json
+# tag value - shankar or christine
+# timestamp - unix timestamp
+
+
+# read json
+# format and push to influxdb
+# print some statistics during the process
+
