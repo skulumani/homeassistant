c01d03b7b9f164e94a07852b24273af6832f9896
Shankar Kulumani <shanks.k@gmail.com>
2021-08-07 15:54:36 -0400

    Keep videos for 20 days

--- record_delete.sh
+++ record_delete.sh
@@ -6,6 +6,6 @@ LQDIR="/media/shankar/usbdrive/video/low" # Do not include tail '/' (bash 'find'
 HQDIR="/media/shankar/usbdrive/video/high" # Do not include tail '/'
 
 ## Delete LQ files older than 7 days -mtime X is files strictly greater than X days old
-find ${LQDIR} -maxdepth 1 -type f -mtime +10 -exec rm {} \;
+find ${LQDIR} -maxdepth 1 -type f -mtime +20 -exec rm {} \;
 ## Delete HQ files older than 720 minutes
 find ${HQDIR} -maxdepth 1 -type f -mtime +0 -exec rm {} \;
