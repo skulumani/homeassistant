f8f473a57f804eb904ee8def617ed579a961fb1f
Shankar Kulumani <shanks.k@gmail.com>
2021-10-11 10:28:51 -0400

    Update timelapse

--- timelapse_day.sh
+++ timelapse_day.sh
@@ -11,9 +11,9 @@ IMAGE_DIR="/media/shankar/data/timelapse/"
 # get date using $(date -d "last month" +"%Y%m%d") or equivalent
 
 # ffmpeg -r 30 -pattern_type glob -i 20211010T*.jpg -vcodec libx264 -crf 18 -pix_fmt yuv420p timelapse.mp4
-
-ffmpeg -r 10 -pattern_type glob -i "${IMAGE_DIR}${YESTERDAY}*_FRONT.jpg" -vcodec libx265 -crf 28 -pix_fmt yuv420p ${IMAGE_DIR}${YESTERDAY}_FRONT.mp4
-ffmpeg -r 10 -pattern_type glob -i "${IMAGE_DIR}${YESTERDAY}*_BACK.jpg" -vcodec libx265 -crf 28 -pix_fmt yuv420p ${IMAGE_DIR}${YESTERDAY}_BACK.mp4
+# libx265 - seems to fail for signal attachments
+ffmpeg -r 10 -pattern_type glob -i "${IMAGE_DIR}${YESTERDAY}*_FRONT.jpg" -vcodec libx264 -crf 20 -pix_fmt yuv420p ${IMAGE_DIR}${YESTERDAY}_FRONT.mp4
+ffmpeg -r 10 -pattern_type glob -i "${IMAGE_DIR}${YESTERDAY}*_BACK.jpg" -vcodec libx264 -crf 20 -pix_fmt yuv420p ${IMAGE_DIR}${YESTERDAY}_BACK.mp4
 
 # delete images used 
 # rm "${IMAGE_DIR}${YESTERDAY}*.jpg"
