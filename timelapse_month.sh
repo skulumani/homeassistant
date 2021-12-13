17846a2b84e4fd8b8fde71e0947d099a532c8485
Shankar Kulumani <shanks.k@gmail.com>
2021-12-13 16:03:50 -0500

    Fix timelapse month script

--- timelapse_month.sh
+++ timelapse_month.sh
@@ -10,20 +10,19 @@ IMAGE_DIR="/media/shankar/data/timelapse/"
 FRONT_FILES="${IMAGE_DIR}front.txt"
 BACK_FILES="${IMAGE_DIR}back.txt"
 
-for filename in "${IMAGE_DIR}${THIS_MONTH}"*_FRONT.mp4; do
+for filename in "${IMAGE_DIR}${LAST_MONTH}"*_FRONT.mp4; do
     [ -e "$filename" ] || continue
     echo "file '${filename}'" >> ${FRONT_FILES}
 done
 
-for filename in "${IMAGE_DIR}${THIS_MONTH}"*_BACK.mp4; do
+for filename in "${IMAGE_DIR}${LAST_MONTH}"*_BACK.mp4; do
     [ -e "$filename" ] || continue
     echo "file '${filename}'" >> ${BACK_FILES}
 done
 
 # concat mp4s
-# TODO Not working yet
-ffmpeg -r 30 -f concat -safe 0 -i ${FRONT_FILES} -vcodec libx264 -crf 28 -preset veryslow -pix_fmt yuv420p ${IMAGE_DIR}${THIS_MONTH}_FRONT.mp4
-ffmpeg -r 30 -f concat -safe 0 -i ${BACK_FILES} -vcodec libx264 -crf 28 -preset veryslow -pix_fmt yuv420p ${IMAGE_DIR}${THIS_MONTH}_BACK.mp4 
+ffmpeg -r 30 -f concat -safe 0 -i ${FRONT_FILES} -vcodec libx264 -crf 28 -preset veryslow -pix_fmt yuv420p ${IMAGE_DIR}${LAST_MONTH}_FRONT.mp4
+ffmpeg -r 30 -f concat -safe 0 -i ${BACK_FILES} -vcodec libx264 -crf 28 -preset veryslow -pix_fmt yuv420p ${IMAGE_DIR}${LAST_MONTH}_BACK.mp4 
 
 # delete mp4s
 
@@ -32,5 +31,8 @@ ffmpeg -r 30 -f concat -safe 0 -i ${BACK_FILES} -vcodec libx264 -crf 28 -preset 
 # # rm "${IMAGE_DIR}${YESTERDAY}*.jpg"
 # find ${IMAGE_DIR} -type f -name "${YESTERDAY}*.jpg" -exec rm {} +
 
-# rm ${FRONT_FILES}
-# rm ${BACK_FILES}
+# Delete file list documents
+rm ${FRONT_FILES}
+rm ${BACK_FILES}
+
+# Upload to youtube
